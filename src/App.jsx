57047c7172dccef3dff487d1195aaf5612068b96
güven oruçtutan <guvenoructuan@gmail.com>
2024-08-17 17:52:18 +0300

    İlk yükleme

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import React from "react";
+import HomePage from "./pages/HomePage";
+//import ShopPage from "./pages/ShopPage";
+//import ContactPage from "./pages/ContactPage";
+//import AuthPage from "./pages/AuthPage";
+//import CartPage from "./pages/CartPage";
+// import BlogPage from "./pages/BlogPage";
+ //import BlogDetailsPage from "./pages/BlogDetailsPage";
+//import ProductDetailsPage from "./pages/ProductDetailsPage";
+function App() {
+  return (
+    <React.Fragment>
+      <HomePage />
+      {/* <ShopPage /> */}
+      {/* <ContactPage /> */}
+      {/* <AuthPage /> */}
+      {/* <CartPage /> */}
+  {/* <BlogDetailsPage /> */}
+  {/* <ProductDetailsPage /> */}
+    </React.Fragment>
+  );
+}
+
+export default App;
